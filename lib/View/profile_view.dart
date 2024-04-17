c87708a015c9e5d40d9c9d441680f80ba66232b1
Htet Thura Zaw (Raymond) <htetthurazaw@gmail.com>
2024-04-17 22:41:16 +0800

    added user profile listing

--- lib/View/profile_view.dart
+++ lib/View/profile_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:shiok_jobs_flutter/Bloc/profile_bloc.dart';
 import 'package:shiok_jobs_flutter/Data/response/api_response.dart';
 import 'package:shiok_jobs_flutter/Data/response/user_profile_response.dart';
@@ -38,12 +39,8 @@ class _ProfilePageState extends State<ProfilePage> {
                       case Status.loading:
                         return const Center(child: CircularProgressIndicator());
                       case Status.completed:
-                        return Column(
-                          children: [
-                            showUserProfile(
-                                snapshot.data!.data as UserProfileResponse),
-                          ],
-                        );
+                        return showUserProfile(
+                            snapshot.data!.data as UserProfileResponse);
                       case Status.error:
                         return Text(snapshot.data?.message.toString() ?? '');
                       case null:
@@ -59,12 +56,12 @@ class _ProfilePageState extends State<ProfilePage> {
 
   Widget showUserProfile(UserProfileResponse userProfileResponse) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(userProfileResponse.fullName.toString(),
                   style: const TextStyle(
@@ -76,7 +73,8 @@ class _ProfilePageState extends State<ProfilePage> {
               ),
               Text(
                 "${userProfileResponse.isSeekingJob! ? "" : "Not "}Open to Jobs",
-                style: const TextStyle(color: Color.fromRGBO(116, 219, 251, 1)),
+                style:
+                    const TextStyle(color: Color.fromRGBO(116, 219, 251, 1)),
               ),
               const SizedBox(
                 height: 10,
@@ -98,6 +96,7 @@ class _ProfilePageState extends State<ProfilePage> {
           ),
         ),
         Expanded(
+          flex: 1,
           child: ListView.builder(
               itemCount: userProfileResponse.workExperiences?.length,
               itemBuilder: (BuildContext context, int index) {
@@ -115,24 +114,24 @@ class _ProfilePageState extends State<ProfilePage> {
                     ]);
               }),
         ),
-        Expanded(
-            child: ListView.builder(
-                itemCount: userProfileResponse.educationHistories?.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                            userProfileResponse
-                                    .educationHistories?[index].school
-                                    .toString() ??
-                                "",
-                            style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 28,
-                                fontWeight: FontWeight.bold)),
-                      ]);
-                }))
+        // Expanded(
+        //     child: ListView.builder(
+        //         itemCount: userProfileResponse.educationHistories?.length,
+        //         itemBuilder: (BuildContext context, int index) {
+        //           return Column(
+        //               crossAxisAlignment: CrossAxisAlignment.start,
+        //               children: [
+        //                 Text(
+        //                     userProfileResponse
+        //                             .educationHistories?[index].school
+        //                             .toString() ??
+        //                         "",
+        //                     style: const TextStyle(
+        //                         color: Colors.black,
+        //                         fontSize: 28,
+        //                         fontWeight: FontWeight.bold)),
+        //               ]);
+        //         }))
       ],
     );
   }
